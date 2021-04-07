adf92c863e976f45122df48c0f76a98a4dfcd8a8
RobinHeath-Albuquerque <79651450+RobinHeath-Albuquerque@users.noreply.github.com>
2021-04-07 10:52:41 -0600

    Adding corretly linked files and superheroes to database

--- superheroes/mysuperheroes/admin.py
+++ superheroes/mysuperheroes/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import SuperHero
-admin.site.register(SuperHero)
-
 
 # Register your models here.
+admin.site.register(SuperHero)
--- superheroes/mysuperheroes/apps.py
+++ superheroes/mysuperheroes/apps.py
@@ -1,14 +1,5 @@
 from django.apps import AppConfig
-from django.urls import path
-from . import views
 
 
 class MysuperheroesConfig(AppConfig):
     name = 'mysuperheroes'
-
-
-app_name = 'superheroes'
-urlpatterns = [
-    path('index', views.index, name='index')
-]
-
